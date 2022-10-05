af54af2380792af6910199bb5c01772600e356a0
nameose <34715603@naver.com>
2022-10-06 03:23:32 +0900

    update

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
+//chart
 $(function () {
   const animation = $(".animation");
   const chartBox = $(".animation .chart");
   const progressOst = $(".animation").offset().top - 600;
-
   $(window).scroll(function () {
     if ($(window).scrollTop() >= progressOst) {
       if (!animation.hasClass("act")) {
@@ -17,19 +17,37 @@ $(function () {
       let title = $this.find("h3");
       let targetNum = title.attr("data-num");
       let circle = $this.find("circle");
-
       $({ rate: 0 }).animate(
         { rate: targetNum },
         {
-          duration: 2500,
+          duration: 3000,
           progress: function () {
             let now = this.rate;
             let amount = 630 - (630 * now) / 100;
-            title.text(Math.floor(now));
+            title.text(Math.floor(now) + "%");
             circle.css({ strokeDashoffset: amount });
           },
         }
       );
     });
   }
+});
+
+//topbtn
+const btt = document.querySelector("#back-to-top");
+let scrollAmount;
+console.log(window);
+window.addEventListener("scroll", function () {
+  scrollAmount = this.scrollY;
+  if (scrollAmount > scrollAmount / 4) {
+    btt.classList.add("visible");
+  } else {
+    btt.classList.remove("visible");
+  }
+});
+btt.addEventListener("click", function (e) {
+  e.preventDefault();
+  document
+    .querySelector(btt.getAttribute("href"))
+    .scrollIntoView({ behavior: "smooth" });
 });
